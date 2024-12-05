a3b17992bcc005920b98510d58d29a6abcfe6c5f
enfmarinho <eduardonestormarinho228@gmail.com>
2024-12-04 22:04:01 -0300

    Implementing stuff

--- LSystem.py
+++ LSystem.py
@@ -10,11 +10,16 @@ def validate(string):
     return True
 
 
-def apply_production_rules(l_system, n_iterations):
+def apply_production_rules(l_system, rules, n_iterations):
     if n_iterations == 0:
         return l_system
 
-    # TODO make the substitutions
-    next_iteration_l_system = l_system
+    # TODO probably it would be more efficient to use a list of chars instead of strings, since strings are immutable
+    next_iteration_l_system = ""
+    for char in l_system:
+        if char in rules:
+            next_iteration_l_system += rules[char]
+        else:
+            next_iteration_l_system += char
 
-    return apply_production_rules(next_iteration_l_system, n_iterations - 1)
+    return apply_production_rules(next_iteration_l_system, rules, n_iterations - 1)
--- main.py
+++ main.py
@@ -11,6 +11,19 @@ def read_input():
     )
 
 
+def read_rules():
+    print("Escreva as regras a seguir no seguinte formato: char->string")
+    print("Quando não houver mais regras deixe a linha em branco!")
+
+    rules = {}
+    while True:
+        line = input("Regra: ")
+        if line == "":
+            return rules
+        strings = line.split("->", 1)
+        rules[strings[0]] = strings[1]
+
+
 def main():
     n_iterations, angle, width_increment, l_system = read_input()
 
@@ -18,7 +31,9 @@ def main():
         print("Cadeia de caracteres L-System inválida!")
         return False
 
-    l_system = ls.apply_production_rules(l_system, n_iterations)
+    l_system = ls.apply_production_rules(l_system, read_rules(), n_iterations)
+
+    print(f"L-System após {n_iterations} reescritas: {l_system}")
 
     Renderer(angle, width_increment).render(l_system)
 
