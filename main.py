a19de286481e9c9e37d86353bfea0f8ddc3a681f
enfmarinho <eduardonestormarinho228@gmail.com>
2024-12-04 22:38:52 -0300

    Adding functionality.

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
+import sys
+
 import LSystem as ls
 from renderer import Renderer
 
 
-def read_input():
+def read_render_input():
     return (
-        int(input("Número de iterações: ")),
+        int(input("Número de reescritas: ")),
         int(input("Ângulo: ")),
         float(input("Incremento da largura: ")),
         input("L-System: "),
+    )
+
+
+def read_check_input():
+    return (
+        int(input("Número de reescritas: ")),
+        input("L-System: "),
+        input("L-System esperado: "),
     )
 
 
@@ -25,17 +35,40 @@ def read_rules():
 
 
 def main():
-    n_iterations, angle, width_increment, l_system = read_input()
-
-    if not ls.validate(l_system):
-        print("Cadeia de caracteres L-System inválida!")
+    argv = sys.argv
+    if len(argv) < 2:
+        print("Argumento faltando. Use checar ou renderizar")
         return False
 
-    l_system = ls.apply_production_rules(l_system, read_rules(), n_iterations)
+    elif argv[1] == "checar":
+        n_iterations, l_system, expected_l_system = read_check_input()
+        final_l_system = ls.apply_production_rules(l_system, read_rules(), n_iterations)
 
-    print(f"L-System após {n_iterations} reescritas: {l_system}")
+        if final_l_system == expected_l_system:
+            print(
+                f"Verdadeiro: O L-System esperado é equivalente ao L-System após {n_iterations} reescritas"
+            )
+        else:
+            print(
+                f"Falso: O LSystem esperado não é equivalente ao LSystem após {n_iterations} reescritas"
+            )
 
-    Renderer(angle, width_increment).render(l_system)
+    elif argv[1] == "renderizar":
+        n_iterations, angle, width_increment, l_system = read_render_input()
+
+        if not ls.validate(l_system):
+            print("Cadeia de caracteres L-System inválida!")
+            return False
+
+        l_system = ls.apply_production_rules(l_system, read_rules(), n_iterations)
+
+        print(f"L-System após {n_iterations} reescritas: {l_system}")
+
+        Renderer(angle, width_increment).render(l_system)
+
+    else:
+        print("Argumento inválido!")
+        return False
 
     return True
 
