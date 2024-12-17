c19343cd1799fba9151c80e4d830f92e8ad9a657
enfmarinho <eduardonestormarinho228@gmail.com>
2024-12-17 20:27:50 -0300

    Adding 'G' and 'g' variables to the alphabet.

--- src/alphabet.py
+++ src/alphabet.py
@@ -4,6 +4,8 @@ LINE_SIZE = 30
 ALPHABET = {
     "F": lambda: t.forward(LINE_SIZE),
     "f": lambda: t.forward(LINE_SIZE / 2),
+    "G": lambda: t.forward(LINE_SIZE),
+    "g": lambda: t.forward(LINE_SIZE / 2),
     "[": "This is an edge case",  # TODO improve this implementation avoiding this edge case
     "]": "This is an edge case",
     "r": lambda: t.color("red"),
